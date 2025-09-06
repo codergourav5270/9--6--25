502efd7cdb058ce75b2422a2ffd50cd5bfd2c10e
Gourav Singh <gourav5270singh@gmail.com>
2025-09-06 17:14:02 +0530

    i change in idx 3rd line.js

--- table-by-mapfunction/style.js
+++ table-by-mapfunction/style.js
@@ -1,4 +1,4 @@
 let num = Number(prompt("enter any no."))
 let arr = [1,2,3,4,5,6,7,8,9,10];
-let table = arr.map(`${num} * ${idx} = ${num*idx}`);
+let table = arr.map(`${num} * ${i} = ${num*i}`);
 console.log(table.join("\n"));
